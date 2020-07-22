2faf518480c40df23acd41c585f8bb627606dcc3
mseewer <marco.seewer@gmail.com>
2020-07-22 14:17:19 +0200

    test audio player

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
 from mycroft.skills.audioservice import AudioService
+from mycroft.audio.services.vlc import VlcService
 
 import requests
 
@@ -16,28 +17,27 @@ def getURL(message):
             return URLS[url]
     return default
 
-def find_mime(url):
-    mime = 'audio/mpeg'
-    response = requests.Session().head(url, allow_redirects=True)
-    if 200 <= response.status_code < 300:
-        mime = response.headers['content-type']
-    return mime
+# def find_mime(url):
+#     mime = 'audio/mpeg'
+#     response = requests.Session().head(url, allow_redirects=True)
+#     if 200 <= response.status_code < 300:
+#         mime = response.headers['content-type']
+#     return mime
 class RadioPlayer(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.mediaplayer = VlcService(config={'low_volume': 10, 'duck': True})
     
-    def initialize(self):
-        self.audio_service = AudioService(self.bus)
 
     @intent_file_handler('player.radio.intent')
     def handle_player_radio(self, message):
         self.speak_dialog('player.radio')
-
+        tracklist = []
         url = getURL(message)
-        mime = find_mime(url)
-        backends = self.audio_service.available_backends()
-        self.log.debug(backends)
-        self.audio_service.play( (url, mime) )
+        # mime = find_mime(url)
+        tracklist.append(self.stream_url)
+        self.mediaplayer.add_list(tracklist)
+        self.mediaplayer.play()
 
 
 
