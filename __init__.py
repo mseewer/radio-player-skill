d6ec544fb6f954181acee4683c6630efcb579dca
mseewer <marco.seewer@gmail.com>
2020-07-22 14:01:00 +0200

    test audio player

--- __init__.py
+++ __init__.py
@@ -1,32 +1,44 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.skills.audioservice import AudioService
 
-import vlc
+import requests
 
 
+URLS = {
+    "rro" : "http://streaming.swisstxt.ch/m/rro/aac_32"
+
+}
+
+def getURL(message):
+    default = URLS["rro"] #default value
+    for url in URLS:
+        if url in message: 
+            return URLS[url]
+    return default
+
+def find_mime(url):
+    mime = 'audio/mpeg'
+    response = requests.Session().head(url, allow_redirects=True)
+    if 200 <= response.status_code < 300:
+        mime = response.headers['content-type']
+    return mime
 class RadioPlayer(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+    
+    def initialize(self):
+        self.audio_service = AudioService(self.bus)
 
     @intent_file_handler('player.radio.intent')
     def handle_player_radio(self, message):
         self.speak_dialog('player.radio')
-        url = 'http://streaming.swisstxt.ch/m/rro/aac_32'
 
-        #define VLC instance
-        instance = vlc.Instance()
+        url = getURL(message)
+        mime = find_mime(url)
+        backends = self.audio_service.available_backends()
+        self.log.debug(backends)
+        self.audio_service.play( (url, mime) )
 
-        #Define VLC player
-        player=instance.media_player_new()
-
-        #Define VLC media
-        media=instance.media_new(url)
-
-        #Set player media
-        player.set_media(media)
-
-        #Play the media
-        player.play()
-        input()
 
 
 def create_skill():
